a1646730c6e3482237f748396f83b4b11d8d6f77
Titux Metal (DEV) <github@lgdweb.fr>
2024-11-01 09:55:09 +0100

    build: Integrate astro-icon for custom icon support
    - Added `astro-icon` integration to the Astro configuration.
    - Configured `iconDir` to `src/assets/icons` for custom icon management.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
+import icon from 'astro-icon'
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), icon({ iconDir: 'src/assets/icons' })],
   output: 'server',
   security: {
     checkOrigin: true
